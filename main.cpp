e1e0c64faec49f12fb745a51e7653b5c495bb1d5
Milena Moyano <mileanahimm039@gmail.com>
2026-09-13 01:19:06 -0500

    implementar reporte general y playlist de 1M vistas

--- main.cpp
+++ main.cpp
@@ -3,12 +3,12 @@
 
 using namespace std;
 void mostrarMenu (){
-    cout << "============ SISTEMA DE GESTION DE MUSICA ============"<<endl;
+    cout << "============ SISTEMA DE GESTION DE MUSICA (⁠◍⁠•⁠ᴗ⁠•⁠◍⁠)⁠============"<<endl;
     cout << "1. Registrar una canción" <<endl;
     cout << "2. Mostrar los registros de canciones"<<endl; 
     cout << "3. Buscar una canción "<<endl; 
     cout << "4. Mostrar reporte general"<<endl; 
-    cout << "5. Playlist para barrer la casa como alma depresiva"<<endl; 
+    cout << "5. Playlist para barrer la casa como señora depechada "<<endl; 
     cout << "6. Salir" <<endl;
     cout << "seleccione una opción: ";
 } 
@@ -44,7 +44,7 @@ int main (){
                     cout<<"No hay canciones registradas :(\n"; 
                 }else{
                     for (int i = 0; i < cantidadCanciones; i++){
-                        cout (i+1)<<"*"<<nombres[i];
+                        cout<<(i+1)<<" ⁠♡ "<<nombres[i];
                         cout<<"-"<<autores[i];
                         cout<<"("<<visitas[i]<<"visitas en Youtube\n)"; 
                     }
@@ -60,11 +60,11 @@ int main (){
                     cin>> buscar; 
                     bool encontrada = false;
                     for (int i = 0; i < cantidadCanciones; i++){
-                        if (nombres=busqueda);{
+                        if (nombres[i]==buscar);{
                             cout<< " Encontramos tu canción :D!\n";
                             cout<< "Nombre: "<<nombres[i]<<endl; 
-                            cout<<"Autor: "<<autores[i]<<endl;
-                            cout<<"Visitas en Youtube: "<<visitas[i]<<endl; 
+                            cout<< "Autor: "<<autores[i]<<endl;
+                            cout<< "Visitas en Youtube: "<<visitas[i]<<endl; 
                             encontrada=true:
                         }   
                     }
@@ -72,15 +72,49 @@ int main (){
                 break;
             case 4:
                 cout<<"\nReporte general\n";
+                if (cantidadCanciones==0) {
+                    cout <<"No hay canciones registradas :(\n";
+                }else{
+                    int totalVisitas=0;
+                    int masVistas=0;
+                    for (int i = 0; i < cantidadCanciones; i++){
+                        totalVisitas += visitas[i]; 
+                        if (visitas[i]>visitas[masVistas]){
+                            masVistas=i;
+                        }  
+                    }
+                    float promedioVisitas = (float)totalVisitas / cantidadCanciones;
+                    cout<<"Total de canciones registradas"<<cantidadCanciones<<endl;
+                    cout << "Total de visitas acumuladas: " << totalVisitas << " reproducciones" << endl;
+                    cout << "Promedio de visitas por cancion: " << promedioVisitas << " reproducciones" << endl;
+                    cout << "Cancion mas popular: " << nombres[masVistas]<< " de " << autores[masVistas]<< " con " << visitas[masVistas] << " visitas." << endl;
+                } 
                 break;
             case 5: 
                 cout<<"\nPlaylist para barrer la casa como alma depresiva\n";
+                if (cantidadCanciones==0){
+                    cout<< "No hay canciones registradas :(\n";
+                }else{
+                    int limiteVisitas = 1000000;
+                    bool existenExitos = false;
+                    cout<<"Buscando los hits mas populares para ti ;)...";
+                    for (int i = 0; i < cantidadCanciones; i++){
+                        if (visitas[i]>=limiteVisitas){
+                            cout<<" ♡ "<<nombres[i]<<" - "<<autores[i]<< "("<<visitas[i]<<" visitas)\n";
+                            existenExitos = true;
+                        }
+                        }
+                    }
+                    if (!existenExitos)
+                    {
+                        cout<< "\nNo tienes canciones con mas de 1 millon de visitas en Youtube. !Registralo :D!\n"
+                    } 
                 break;
             case 6:
                 cout<< "\nSaliendo del Programa: Chao :D\n";
                 break;
             default:
-                cout<< "\nOpcion invalida. Por favor intente de nuevo\n";
+                cout<< "\nOpcion invalida. Por favor intente de nuevo ._.\n";
                 break;
             }
     }while (opcion !=6);
